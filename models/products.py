a3c7f36b6e01d16f78a0186d7d38a8d396553170
Fyodor <chasolist@gmail.com>
2026-02-12 21:15:47 +0500

    Add pagenation in Product

--- models/products.py
+++ models/products.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey, text
+from datetime import datetime
+from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey, text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship  
 from app.database import Base
 
@@ -13,6 +14,8 @@ class Product(Base):
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)  
     category: Mapped["Category"] = relationship("Category", back_populates="products")  
